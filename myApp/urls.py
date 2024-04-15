f4f71889375da0960d9622b82c97404bd39e176b
Nicolas Muñoz Galvez <nico.munozg@duocuc.cl>
2024-04-15 02:53:56 -0400

    se crea ver y crear libro
    funcionando la funcion de crear libros que crea en la base de datos los libros y la funcion de ver el detalle de los libros

--- myApp/urls.py
+++ myApp/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('inicio',inicio,name="inicio"),
     path('libro',libro,name="libro"),
     path('<int:id>/editar/', views.editar_libro, name='editar_libro'),
-    path('<int:id>/eliminar/', views.eliminar_libro, name='eliminar_libro')
+    path('<int:id>/eliminar/', views.eliminar_libro, name='eliminar_libro'),
+    path('crear', views.crear_libro, name='crear_libro'),
+    path('<int:id>/', views.detalle_libro, name='detalle_libro'),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Libro, Categoria, CategoriaUsuario, Usuario
+from django.contrib import messages
 
 
 # Create your views here.
@@ -26,12 +27,12 @@ def libro(request):
 def crear_libro(request):
     if request.method == "POST":
         categoria_id = request.POST.get('categoria')
-        categoria = get_object_or_404(categoria, id=categoria_id)
+        categoria = get_object_or_404(Categoria, id=categoria_id)
 
         codigo = request.POST.get('codigo')
         nombre = request.POST.get('nombre')
         descripcion = request.POST.get('descripcion')
-        imagen = request.POST.get('imagen')
+        imagen = request.FILES.get('imagen')
 
         Libro.objects.create(
             codigo_isbn=codigo,
@@ -40,6 +41,10 @@ def crear_libro(request):
             imagen=imagen,
             categoria=categoria
         )
+        messages.success(request, 'Se ha creado el libro correctamente')
+
+        return redirect('libro')
+
 
     categorias = Categoria.objects.all()
 
@@ -64,6 +69,8 @@ def editar_libro(request, id):
             libro.imagen = request.FILES['imagen']
 
         libro.save()
+        messages.success(request, 'Se ha editado el libro correctamente')
+        return redirect('libro')
 
     categorias = Categoria.objects.all()
     context = {
@@ -74,9 +81,21 @@ def editar_libro(request, id):
     return render(request, 'html_apps/editar.html', context)
 
 
+def detalle_libro(request, id):
+    libro = get_object_or_404(Libro, id=id)
+
+    context = {
+        'libro': libro
+    }
+
+    return render (request, 'html_apps/detalle.html', context)
+
 def eliminar_libro(request, id):
     libro = get_object_or_404(Libro, id=id)
     libro.delete()
+
+    messages.success(request, 'Se ha eliminado el libro correctamente')
+
 
     return redirect('libro')
        
