8dca6e1b16070dc447c71106e1f45a2763ae79a5
ni <revecoFuentesd@gmail.com>
2024-04-22 16:54:53 -0400

    eliminando

--- myApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Tipo_usuario,Usuario,Categoria,Libro
-
-# Register your models here.
-admin.site.register(Tipo_usuario)
-admin.site.register(Usuario)
-admin.site.register(Categoria)
-admin.site.register(Libro)
